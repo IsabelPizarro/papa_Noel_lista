938034a533bf4f8cdd839498af999e4fb5c912b7
IsabelPizarro <chabelipizarro1@gmail.com>
2019-10-21 20:42:50 +0200

    add list component2

---
+++ papa-noel-list/src/AddWhist.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import TextField from "@material-ui/core/TextField";
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1)
+  },
+  dense: {
+    marginTop: theme.spacing(2)
+  },
+  menu: {
+    width: 600
+  }
+}));
+
+ class AddWhist extends React.Component {
+ 
+  constructor() {
+    super();
+    
+    this.handleChange=this.handleChange.bind(this);
+    this.handleText=this.handleText.bind(this);
+   
+  }
+  handleChange(event) {
+    
+    console.log(event.currentTarget.value);
+    
+  }
+  handleText(event){
+  
+  
+    console.log(event.currentTarget.value);
+  }
+
+  render(){
+  const {handleChange, handleText} = this.props;
+  
+
+  
+
+  return (
+    <form className="form" noValidate autoComplete="off">
+      <TextField
+        id="outlined-name"
+        label="Introduce tu deseo"
+        className="text"
+        
+        onChange={handleText}
+        type="text"
+        margin="normal"
+        variant="outlined"
+      /> 
+     
+      <Button variant="contained" color="primary" value="enviar"  onClick={handleChange}>
+     Añadir
+    </Button>
+    </form>
+  );
+}
+}
+export default AddWhist;
--- papa-noel-list/src/App.js
+++ papa-noel-list/src/App.js
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import ListWhist from './ListWhist';
+import AddWhist from "./AddWhist";
 import './App.css';
 
-function App() {
+
+class App extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wish:"",
+     whishes:[]
+     
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleText = this.handleText.bind(this);
+  }
+  
+ 
+   
+  handleText (event) {
+    
+    const choice=event.currentTarget.value;
+    console.log(choice);
+    
+    this.setState({wish:choice,
+      
+  });
+}
+  handleChange (event) {
+    event.preventDefault();
+    debugger;
+    if (this.state.wish === "") {
+      return "";
+    }
+    
+    
+    console.log(event.currentTarget.value);
+    this.state.whises.push(this.state.wish);
+    
+    this.setState({
+      whises:this.state.whises,
+      wish:""
+    });
+    
+    
+    
+
+
+   console.log(this.setState.wishes) 
+console.log(this.state.wish);    
+console.log(this.state.wishes);   
+
+      
+  
+ }
+
+
+
+
+
+  render(){
+    const {wish, whishes}=this.state;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Lista de Papa Noel</h1>
+     
+    
       </header>
+      <main>
+        <AddWhist 
+        handleText={this.handleText}
+      
+        handleChange={this.handleChange}/>
+        
+        <ListWhist />
+      </main>
     </div>
   );
 }
-
+}
 export default App;
